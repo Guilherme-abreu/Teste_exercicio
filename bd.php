b2ab39b274515bb4bf78d79a226465458e509614
Guilherme-abreu <gulermeabreu123@gmail.com>
2024-09-16 14:58:41 -0300

    aula to testando

---
+++ bd.php
@@ -0,0 +1,36 @@
+<?php
+
+//configuração do banco de dados
+
+$host = 'localhost';
+$dbname = 'livro';
+$username = 'root';
+$password = '';
+
+//criar a conexão com o banco de dados
+
+$conn = new mysql($host,$username,$password,$dbname);
+
+//verificar se houve erro na conexão
+
+if($conn->conect_error){
+    die("Falha na conexão",$conn->conect_error);
+}
+echo "Conexão realizada com sucesso";
+
+//Dados a serem inseridos
+
+$sql = "INSERT INTO autores (nome,nacionalidade,datanascimento) VALUES ('Machado de Assis', 'Brasil',''06/21/39')"
+
+//executo a instrução e verificor erro 
+
+if($conn->query($sql) === TRUE){
+    ECHO "DADOS INSERIDOS COM SUCESSO";
+}else{
+    echo "Erro ao inserir dados",$conn->error;
+
+}
+
+$conn->close()
+
+?>
